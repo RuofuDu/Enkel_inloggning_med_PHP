f01615e4b0501b75aef1c2035b1663ef11b9e93d
Ruofu Du <ruofu.du@skola.taby.se>
2022-09-09 07:52:26 +0200

    change

--- inloggning.php
+++ inloggning.php
@@ -3,9 +3,15 @@
 
 <body>
 
-
+    <form action="inloggning.php" method="post">
+        Användernamn: <input type="text" name="anvandernamn">
+        Lösenord: <input type="password" name="password">
+        <input type="submit">
+    </form>
     <?php
     echo "Hello World!";
+    $anvandernamn = $_POST["anvandernamn"];
+    $losenord = $_POST["password"];
     ?>
 
 </body>
